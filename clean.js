7661dce416149f5979f703eeccf4cf790978ae77
foydalicantent-hub <foydalicantent@gmail.com>
2026-08-06 15:50:33 +0500

    Update import paths in clean.js

--- clean.js
+++ clean.js
@@ -1,7 +1,7 @@
-import { Memory } from "../Memory.js";
-import { getOrCreateUser } from "../userService.js";
-import { ensureLanguage } from "../User.js";
-import { t } from "../utils/i18n.js";
+import { Memory } from "./Memory.js";
+import { getOrCreateUser } from "./userService.js";
+import { ensureLanguage } from "./User.js";
+import { t } from "./utils/i18n.js";
 
 export async function cleanCommand(ctx) {
   const user = await getOrCreateUser(ctx);
