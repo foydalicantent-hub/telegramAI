b31dd8683718e3debbc64d59248f261b18681e54
foydalicantent-hub <foydalicantent@gmail.com>
2026-08-06 16:40:34 +0500

    Update language.js

--- language.js
+++ language.js
@@ -1,4 +1,7 @@
 import { InlineKeyboard } from "grammy";
+import { getOrCreateUser } from "./userService.js";
+import { ensureLanguage } from "./User.js";
+import { t } from "./utils/i18n.js";
 
 export function languageKeyboard() {
   return new InlineKeyboard()
@@ -6,4 +9,25 @@ export function languageKeyboard() {
     .text("🇷🇺 Русский", "lang_ru")
     .row()
     .text("🇺🇸 English", "lang_en");
+}
+
+export async function languageCommand(ctx) {
+  const user = await getOrCreateUser(ctx);
+  const lang = ensureLanguage(user);
+
+  await ctx.reply(t(lang, "choose_language"), {
+    reply_markup: languageKeyboard(),
+  });
+}
+
+export async function languageCallback(ctx) {
+  const data = ctx.callbackQuery.data;
+  const lang = data.split("_")[1]; // lang_uz -> uz
+
+  const user = await getOrCreateUser(ctx);
+  user.language = lang;
+  await user.save();
+
+  await ctx.answerCallbackQuery();
+  await ctx.editMessageText(t(lang, "language_changed"));
 }
