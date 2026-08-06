6c90492de4f08aeb58b8ac14f17a22fe5fb436ab
foydalicantent-hub <foydalicantent@gmail.com>
2026-08-06 15:48:17 +0500

    Update searchService.js

--- searchService.js
+++ searchService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-import { config } from "../env.js";
-import { logger } from "../logger.js";
+import { config } from "./env.js";
+import { logger } from "./logger.js";
 
 /**
  * Searches the web (via YouTube Data API) and returns a list of
