a45cc5829eb4fc93d124082fc03f387967558888
foydalicantent-hub <foydalicantent@gmail.com>
2026-08-07 17:51:57 +0500

    Refactor startCommand function for clarity

--- start.js
+++ start.js
@@ -1,20 +1,20 @@
-import { mainMenuKeyboard } from "./mainMenu.js";
 import { User } from "./User.js";
+import { mainMenuKeyboard } from "./mainMenu.js";
 
 export async function startCommand(ctx) {
-  const userId = ctx.from.id;
+  const userId = ctx.from.id;
 
-  // /start bosilganda agar eski kutish holati qolgan bo'lsa, uni tozalab tashlaymiz
-  await User.findOneAndUpdate(
-    { telegramId: userId },
-    { waitingForInstruction: false },
-    { upsert: true }
-  );
+  // /start bosilganda eski kutish holatini tozalaymiz
+  await User.findOneAndUpdate(
+    { telegramId: userId },
+    { waitingForInstruction: false },
+    { upsert: true }
+  );
 
-  await ctx.reply(
-    "👋 Assalomu alaykum! Telegram Business va AI yordamchi botiga xush kelibsiz. Kerakli funksiyani tanlang:",
-    {
-      reply_markup: mainMenuKeyboard,
-    }
-  );
-} mana start.js
+  await ctx.reply(
+    "👋 Assalomu alaykum! Telegram Business va AI yordamchi botiga xush kelibsiz. Kerakli funksiyani tanlang:",
+    {
+      reply_markup: mainMenuKeyboard,
+    }
+  );
+}
